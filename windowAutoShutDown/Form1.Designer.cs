e5f6557cd2fb4a607046ec069bf577e17a8d07a9
andyNg <tynshjt@gmail.com>
2016-12-05 20:30:22 +0200

    Dec 5th, 2016

---
+++ windowAutoShutDown/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace windowAutoShutDown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxAction = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numHour = new System.Windows.Forms.NumericUpDown();
+            this.numMint = new System.Windows.Forms.NumericUpDown();
+            this.numSec = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBoxRemind = new System.Windows.Forms.CheckBox();
+            this.numRemind = new System.Windows.Forms.NumericUpDown();
+            this.labelTimeNow = new System.Windows.Forms.Label();
+            this.labelTimeRemain = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonCancle = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numHour)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMint)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSec)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numRemind)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(76, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Auto Shutdown Timer";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBoxAction);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 80);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(304, 89);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Action";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.labelTimeRemain);
+            this.groupBox2.Controls.Add(this.labelTimeNow);
+            this.groupBox2.Controls.Add(this.numRemind);
+            this.groupBox2.Controls.Add(this.checkBoxRemind);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.numSec);
+            this.groupBox2.Controls.Add(this.numMint);
+            this.groupBox2.Controls.Add(this.numHour);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 192);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(303, 145);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Timer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Choose an action";
+            // 
+            // comboBoxAction
+            // 
+            this.comboBoxAction.FormattingEnabled = true;
+            this.comboBoxAction.Items.AddRange(new object[] {
+            "Log off",
+            "Restart",
+            "Shutdown"});
+            this.comboBoxAction.Location = new System.Drawing.Point(6, 46);
+            this.comboBoxAction.Name = "comboBoxAction";
+            this.comboBoxAction.Size = new System.Drawing.Size(270, 21);
+            this.comboBoxAction.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Perform in:";
+            // 
+            // numHour
+            // 
+            this.numHour.Location = new System.Drawing.Point(59, 28);
+            this.numHour.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            this.numHour.Name = "numHour";
+            this.numHour.Size = new System.Drawing.Size(67, 20);
+            this.numHour.TabIndex = 1;
+            // 
+            // numMint
+            // 
+            this.numMint.Location = new System.Drawing.Point(126, 28);
+            this.numMint.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numMint.Name = "numMint";
+            this.numMint.Size = new System.Drawing.Size(67, 20);
+            this.numMint.TabIndex = 2;
+            // 
+            // numSec
+            // 
+            this.numSec.Location = new System.Drawing.Point(193, 28);
+            this.numSec.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numSec.Name = "numSec";
+            this.numSec.Size = new System.Drawing.Size(67, 20);
+            this.numSec.TabIndex = 3;
+            this.numSec.ValueChanged += new System.EventHandler(this.numSec_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(266, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "h : s";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Time";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Remain";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // checkBoxRemind
+            // 
+            this.checkBoxRemind.AutoSize = true;
+            this.checkBoxRemind.Location = new System.Drawing.Point(6, 105);
+            this.checkBoxRemind.Name = "checkBoxRemind";
+            this.checkBoxRemind.Size = new System.Drawing.Size(108, 17);
+            this.checkBoxRemind.TabIndex = 7;
+            this.checkBoxRemind.Text = "Remind (minute): ";
+            this.checkBoxRemind.UseVisualStyleBackColor = true;
+            this.checkBoxRemind.CheckedChanged += new System.EventHandler(this.checkBoxRemind_CheckedChanged);
+            // 
+            // numRemind
+            // 
+            this.numRemind.Location = new System.Drawing.Point(193, 104);
+            this.numRemind.Name = "numRemind";
+            this.numRemind.Size = new System.Drawing.Size(67, 20);
+            this.numRemind.TabIndex = 8;
+            this.numRemind.Visible = false;
+            // 
+            // labelTimeNow
+            // 
+            this.labelTimeNow.AutoSize = true;
+            this.labelTimeNow.Location = new System.Drawing.Point(132, 58);
+            this.labelTimeNow.Name = "labelTimeNow";
+            this.labelTimeNow.Size = new System.Drawing.Size(49, 13);
+            this.labelTimeNow.TabIndex = 9;
+            this.labelTimeNow.Text = "00:00:00";
+            this.labelTimeNow.Visible = false;
+            // 
+            // labelTimeRemain
+            // 
+            this.labelTimeRemain.AutoSize = true;
+            this.labelTimeRemain.Location = new System.Drawing.Point(132, 84);
+            this.labelTimeRemain.Name = "labelTimeRemain";
+            this.labelTimeRemain.Size = new System.Drawing.Size(49, 13);
+            this.labelTimeRemain.TabIndex = 10;
+            this.labelTimeRemain.Text = "00:00:00";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStart.Location = new System.Drawing.Point(343, 126);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(142, 88);
+            this.buttonStart.TabIndex = 11;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonCancle
+            // 
+            this.buttonCancle.Enabled = false;
+            this.buttonCancle.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCancle.Location = new System.Drawing.Point(343, 251);
+            this.buttonCancle.Name = "buttonCancle";
+            this.buttonCancle.Size = new System.Drawing.Size(142, 88);
+            this.buttonCancle.TabIndex = 12;
+            this.buttonCancle.Text = "Cancel";
+            this.buttonCancle.UseVisualStyleBackColor = true;
+            this.buttonCancle.Click += new System.EventHandler(this.buttonCancle_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 362);
+            this.Controls.Add(this.buttonCancle);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Auto Shutdown";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numHour)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMint)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSec)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numRemind)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBoxAction;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numSec;
+        private System.Windows.Forms.NumericUpDown numMint;
+        private System.Windows.Forms.NumericUpDown numHour;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelTimeRemain;
+        private System.Windows.Forms.Label labelTimeNow;
+        private System.Windows.Forms.NumericUpDown numRemind;
+        private System.Windows.Forms.CheckBox checkBoxRemind;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonCancle;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ windowAutoShutDown/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace windowAutoShutDown
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int hour;
+    
+        private int min;
+        private int sec;
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            labelTimeNow.Visible = true;
+            labelTimeNow.Text = DateTime.Now.ToString("hh:mm:ss tt");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // comboBoxAction.SelectedIndex = 0;
+        }
+
+        private void checkBoxRemind_CheckedChanged(object sender, EventArgs e)
+        {
+            numRemind.Visible = true;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if ((numMint.Value == 0) && (numHour.Value == 0) && (numSec.Value==0))
+            {
+                MessageBox.Show("Please select time","Error", MessageBoxButtons.OK);
+            }
+            else
+            {
+                hour = (int)numHour.Value;
+                min = (int)numMint.Value;
+                sec = (int)numSec.Value;
+                timer2.Start();
+                timer1.Enabled = true;
+                buttonCancle.Enabled = true;
+                buttonStart.Enabled = false;
+            }
+
+
+        }
+
+        private void buttonCancle_Click(object sender, EventArgs e)
+        {
+            timer2.Stop();
+            buttonStart.Enabled = true;
+            buttonCancle.Enabled = false;
+            labelTimeRemain.Text = "00:00:00";
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+
+            if (hour > 0 || min > 0 || sec > 0)
+            {
+                // change the time/ countdown
+                if (min == 0 && hour > 0)
+                {
+                    min = 59;
+                    hour -= 1;
+                    sec = 60;
+                }
+                if (sec == 0 && min > 0)
+                {
+                    sec = 60;
+                    min -= 1;
+                }
+
+                sec -= 1;
+
+
+                labelTimeRemain.Text = string.Format("{0}:{1}:{2}", formatTime(hour), formatTime(min), formatTime(sec));
+
+
+                // If the checkBox is checked this will show a alert
+                if (checkBoxRemind.Checked)
+                {
+                    string msg = string.Format("Your computer will {0} in {1} minute. Please be alerted and save uncompleted work", comboBoxAction.Text, min);
+                    if (sec == 0 && min == numRemind.Value)
+                        MessageBox.Show(msg, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+
+            }
+            else
+            {
+                timer2.Stop();
+                int select = comboBoxAction.SelectedIndex;
+                switch (select)
+                {
+                    default:
+                    case 0:
+                        // logoff
+                        Process.Start("Shutdown.exe", "-l");
+                        break;
+                    case 1:
+                        //restart
+                        Process.Start("Shutdown.exe", "-r -t 5");
+                        break;
+                    case 2:
+                        //shutdown
+                        Process.Start("Shutdown.exe", "-s -t 5");
+                        break;
+
+                }
+            }
+         
+
+
+
+        } // timer tick
+
+        private string formatTime(int times) // help with layout 0:1:2 -> 00:01:02
+        {
+            string t = times.ToString();
+            if (times < 10)
+            {
+                t = "0" + t;
+            }
+            return t;
+        }
+
+        private void numSec_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    } //class
+} // program
